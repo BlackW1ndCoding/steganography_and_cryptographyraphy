510e358f00b89219951d7b579b1cbf731dfff47c
BlackW1ndCoding <black.wind.coding@gmail.com>
2022-03-15 00:34:44 +0200

    completed project

--- Steganography and Cryptography/task/src/cryptography/CommandsHandler.kt
+++ Steganography and Cryptography/task/src/cryptography/CommandsHandler.kt
@@ -2,66 +2,80 @@ package cryptography
 
 object CommandsHandler {
     private const val FILE_PATTERN = ".+\\.png"
+    private const val PASSWORD_PATTERN = "\\w+"
 
     fun hideCommand() {
         val fileRegex = FILE_PATTERN.toRegex()
+        val passwordRegex = PASSWORD_PATTERN.toRegex()
         var inputPath = ""
         var outputPath = ""
         var message = ""
+        var password = ""
 
         //read input file
-        fun enterInput(){
+        fun enterInput() {
             println("Input image file:")
             val input = readLine()!!
             if (input.matches(fileRegex)) {
                 inputPath = input
-            }else{
+            } else {
                 println("Wrong input file format!!")
                 enterInput()
             }
         }
+
         //read output file
-        fun enterOutput(){
+        fun enterOutput() {
             println("Output image file:")
             val output = readLine()!!
             if (output.matches(fileRegex)) {
                 outputPath = output
-            }else{
+            } else {
                 println("Wrong output file format!!")
                 enterOutput()
             }
         }
+
         //read message
-        fun readMessage(){
+        fun readMessage() {
             println("Message to hide:")
             message = readLine().toString()
         }
 
+        //read password
+        fun readPassword() {
+            println("Password:")
+                password = readln()
+        }
         enterInput()
         enterOutput()
         readMessage()
+        readPassword()
 //        println("Input image: $inputPath")
 //        println("Output image: $outputPath")
 
-        hide(inputPath, outputPath, message)
-
-
+        hide(inputPath, outputPath, message, password)
     }
 
-    fun showCommand(){
+    fun showCommand() {
         val fileRegex = FILE_PATTERN.toRegex()
+        val passwordRegex = PASSWORD_PATTERN.toRegex()
         var inputPath = ""
+        var password = ""
 
-        fun enterInput(){
+        fun enterInput() {
             println("Input image file:")
             inputPath = readLine()!!
-            if (inputPath.matches(fileRegex)){
-                println("Message:")
-                println(show(inputPath))
-            }else{
+
+            if (!inputPath.matches(fileRegex)) {
                 println("Incorrect file name!!")
                 showCommand()
             }
+
+            println("Password:")
+            password = readln()
+            println("Message:")
+            println(show(inputPath, password))
         }
         enterInput()
     }
--- Steganography and Cryptography/task/src/cryptography/Model.kt
+++ Steganography and Cryptography/task/src/cryptography/Model.kt
@@ -4,21 +4,34 @@ import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
 import java.io.IOException
+import java.lang.NumberFormatException
+import java.lang.StringBuilder
 import java.util.*
 import javax.imageio.ImageIO
+import kotlin.experimental.xor
 
-fun hide(inputPath: String, outputPath: String, message: String) {
+fun hide(inputPath: String, outputPath: String, message: String, password: String) {
     val inputFile = File(inputPath)
     val outputFile = File(outputPath)
-    val convertedString = (message + "003").encodeToByteArray()
-        .map { byte -> List(8) { getBit(byte.toInt(), it) }.reversed() }
-        .flatten()
-        .toIntArray()
+    val convertedString = (encryptMessage(message, password) + "\u0000\u0000\u0003".encodeToByteArray())
+        .joinToString("") {
+            if (it.toString(2).length == 8) {
+                it.toString(2)
+            } else {
+                "0".repeat(8 - it.toString(2).length) + it.toString(2)
+            }
+        }
+        .map { it.digitToInt() }
 
     try {
-        val image: BufferedImage = ImageIO.read(inputFile)
+        val sourceImage = ImageIO.read(inputFile)
+        val image = BufferedImage(
+            sourceImage.width, sourceImage.height, BufferedImage.TYPE_INT_RGB
+        )
 
-        if (image.width * image.height < (convertedString.size) * 8) {
+        println("ConvertedString: $convertedString")
+
+        if (image.width * image.height < (convertedString.size)) {
             println("The input image is not large enough to hold this message.")
             return
         }
@@ -29,20 +42,30 @@ fun hide(inputPath: String, outputPath: String, message: String) {
         outer@ while (y < image.height) {
             var x = 0
             while (x < image.width) {
-                val color = Color(image.getRGB(x, y))
-                val bit = convertedString[index]
+                val color = Color(sourceImage.getRGB(x, y))
 
-                image.setRGB(
-                    x++, y,
-                    Color(
-                        color.red,
-                        color.green,
-                        if (bit == 0) color.blue and 1.inv() else color.blue or 1
-                    ).rgb
-                )
-                if (++index > convertedString.lastIndex) {
-                    break@outer
+                if (index <= convertedString.lastIndex) {
+                    val bit = convertedString[index]
+                    val changedColor =
+                        Color(
+                            color.red,
+                            color.green,
+                            color.blue.and(254).or(bit)
+                        ).rgb
+                    println("${color.rgb} ${changedColor} $bit")
+                    image.setRGB(
+                        x, y,
+                        Color(
+                            color.red,
+                            color.green,
+                            color.blue.and(254).or(bit)
+                        ).rgb
+                    )
+                } else {
+                    image.setRGB(x, y, color.rgb)
                 }
+                index++
+                x++
             }
             y++
         }
@@ -56,9 +79,10 @@ fun hide(inputPath: String, outputPath: String, message: String) {
     }
 }
 
-fun show(inputPath: String): String {
+fun show(inputPath: String, password: String): String {
     val inputFile = File(inputPath)
     val bitArray = mutableListOf<Int>();
+    var byteArray = ByteArray(0)
     var outputMessage = ""
 
     try {
@@ -68,22 +92,29 @@ fun show(inputPath: String): String {
         var y = 0
 
         var index = 0
-        loop@ while (!outputMessage.contains("003")) {
+        loop@ while (!outputMessage.contains("\u0000\u0000\u0003")) {
 
             val color = Color(image.getRGB(x++, y))
-            val bit = color.blue.toByte().takeLowestOneBit().toInt()
-            bitArray.add(bit)
+            val bit = color.blue.toUInt() shl 31 shr 31
+            //println("Bit read is $bit")
+            bitArray.add(bit.toInt())
 
-            if (bitArray.size % 8 == 0) {
-                outputMessage = bitArray.chunked(8).map {
-                    it.joinToString("")
-                        .toByte(2)
+            try {
+
+                if (bitArray.size % 8 == 0) {
+                    byteArray = bitArray.chunked(8).map {
+                        it.joinToString("")
+                            .toByte(2)
+                    }.toByteArray()
+
+                    outputMessage = byteArray.toString(Charsets.UTF_8)
                 }
-                    .toByteArray().toString(Charsets.UTF_8)
+            } catch (e: NumberFormatException){
+                //do nothing
             }
             index++
 
-
+            //println("Message $outputMessage")
             if (x == image.width) {
                 x = 0
                 y++
@@ -94,20 +125,37 @@ fun show(inputPath: String): String {
     } catch (e: IOException) {
         println("File can't be read!!")
     }
+
+    byteArray = bitArray.chunked(8).map {
+        it.joinToString("")
+            .toByte(2)
+    }.toByteArray()
     //println("loop end")
-    return outputMessage.dropLast(3)
+    return decryptMessage(byteArray, password)
 }
 
-fun getBit(value: Int, position: Int): Int {
-    return (value shr position) and 1;
-}
+fun encryptMessage(message: String, password: String): ByteArray {
+    val messageToBytes = message.toByteArray()
+    val passwordToBytes = password.toByteArray()
+    var passwordIndex = 0
 
-fun rotateBinary(input: Int): Int {
-    var res = 0;
-    while (input > 0) {
-        res = res shl 1;
-        res = res or (input and 1)
-        input shr 1
+    for (i in messageToBytes.indices){
+        messageToBytes[i] = messageToBytes[i].xor(passwordToBytes[passwordIndex++])
+        if(passwordIndex > passwordToBytes.lastIndex) passwordIndex = 0
     }
-    return res
+
+    return messageToBytes
+}
+
+fun decryptMessage(message: ByteArray, password: String): String{
+    val outputArray = ByteArray(message.size - 3)
+    val passwordArray = password.toByteArray()
+    var passwordIndex = 0
+
+    for(i in 0..message.lastIndex - 3){
+        outputArray[i] = message[i].xor(passwordArray[passwordIndex++])
+        if(passwordIndex > passwordArray.lastIndex) passwordIndex = 0
+    }
+
+    return outputArray.toString(Charsets.UTF_8)
 }
